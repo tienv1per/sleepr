61e5dac519536873d714044109732be0cd782f2e
tienv1per <tienv1perds@gmail.com>
2024-09-15 22:24:08 +0700

    feat: setup users module

---
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  // @IsStrongPassword()
+  @IsString()
+  password: string;
+}
